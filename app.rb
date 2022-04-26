da9590bc4274cc1a8bbb17b3241a5b6c87750b39
0xKubitus <n.cubilier@gmail.com>
2022-04-26 02:54:04 +0200

    next to-do => e)

--- app.rb
+++ app.rb
@@ -6,16 +6,10 @@ require_relative 'lib/player'
 
 #binding.pry
 
-player1 = Player.new("José")
-
-puts player1.name
+player1 = Player.new("Rick")
+player2 = Player.new("Morty")
 puts " "
-p player1.show_state
+p "player1 is #{player1.name}"
+p "player2 is #{player2.name}"
 puts " "
-p player1.gets_damage(8)
-puts " "
-p player1.show_state
-puts " "
-p player1.gets_damage(5)
-puts " "
-
+player1.attacks(player2)
--- lib/player.rb
+++ lib/player.rb
@@ -18,23 +18,25 @@ class Player
 
   # c) Tu dois coder une méthode 'gets_damage' qui prend en entrée un integer => le nombre de dommages subit
   #et qui le soustraie au niveau de vie (@life_points) du joueur:
-  def gets_damage(dmg_input)
-    @life_points = life_points - dmg_input
-    puts "#{name} a perdu #{dmg_input} PV suite à cette attaque !"
+  def gets_damage(attack_dmg)
+    @life_points = life_points - attack_dmg
+    puts "#{name} a perdu #{attack_dmg} PV suite à cette attaque !"
     if @life_points <= 0
       puts "Woopsie! #{name} s'est fait tuer ! (il n'etait pas très brave, le bougre !)"
     end
-  end # fin de la méthode 'gets_damage(dmg_input)
+  end # fin de la méthode 'gets_damage(attack_dmg)
 
 
 
             # ON EN EST LA :
 
 
-  # d) On va coder une méthode 'attacks(playerX)' qui permette de faire qu'un joueur attaque un autre. 
+  # d) On va coder une méthode 'attacks' qui permette de faire qu'un joueur attaque un autre. 
   #Ainsi, si le joueur player1 attaque le joueur player2, on aurait juste à taper player1.attacks(player2):
-  def attack(playerX)
-    # -> ajouter code ici ...
+  def attacks(playerX)
+    puts "Et BIIIM! #{@name} attaque #{playerX.name} :"
+    attack_dmg = compute_damage
+    puts "l'attaque inflige #{attack_dmg} dégats !"
   end # fin de la méthode 'attacks'
 
   # on va créer une méthode à part s'appelant 'compute_damage' :
