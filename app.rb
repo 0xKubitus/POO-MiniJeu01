c90e9d43e1db760be59eec21d8a73d3b8f5b9cea
0xKubitus <n.cubilier@gmail.com>
2022-04-26 03:52:15 +0200

    reste plus que la ptite partie f) corriger le bug

--- app.rb
+++ app.rb
@@ -4,12 +4,33 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-#binding.pry
+
 
 player1 = Player.new("Rick")
 player2 = Player.new("Morty")
 puts " "
-p "player1 is #{player1.name}"
-p "player2 is #{player2.name}"
+
+puts "START=>  Les joueurs sont : #{player1.name} et #{player2.name}"
 puts " "
-player1.attacks(player2)
+
+puts "Voici l'état de chaque joueur :"
+puts player1.show_state
+puts player2.show_state
+puts " "
+
+
+puts "MAINTENANT, PASSONS AU COMBAT !"
+puts " "
+
+# binding.pry
+
+while player1.life_points > 0 && player2.life_points >0 # on fait une boucle while pour faire un combat à mort 
+  player1.attacks(player2)
+  puts " "
+  player2.attacks(player1)
+end
+
+
+
+
+
--- lib/player.rb
+++ lib/player.rb
@@ -1,23 +1,29 @@
 class Player 
-
-  #Un joueur possède 2 attributs : un nom @name (string) et un niveau de vie @life_points (integer):
   attr_accessor :name, :life_points
 
-  # a) Quand on veut créer un objet Player, on ne met que son nom en entrée
-  #(car le niveau de vie est le même pour tout le monde au début => 10 pts de vie).
   def initialize(name)
     @name = name
     @life_points = 10
   end # fin de la méthode 'initialize(name)'
 
-  # b) Code une méthode show_state qui va afficher l'état de l'objet 'Player'
-  #sur laquelle elle est exécutée : "XXXX a YYY points de vie".
+
   def show_state
-    puts "#{name} possède #{life_points} PV."
+    print "-> #{name} a #{life_points} PV."
   end # fin de la méthode 'show_state'
 
-  # c) Tu dois coder une méthode 'gets_damage' qui prend en entrée un integer => le nombre de dommages subit
-  #et qui le soustraie au niveau de vie (@life_points) du joueur:
+
+  def attacks(playerX)
+    
+      puts "BIIIM! #{@name} attaque #{playerX.name} :"
+      attack_dmg = compute_damage
+      puts "l'attaque inflige #{attack_dmg} dégats !"
+      puts " "
+      gets_damage(attack_dmg)
+      show_state
+      puts " "
+  end # fin de la méthode 'attacks'
+
+
   def gets_damage(attack_dmg)
     @life_points = life_points - attack_dmg
     puts "#{name} a perdu #{attack_dmg} PV suite à cette attaque !"
@@ -27,22 +33,9 @@ class Player
   end # fin de la méthode 'gets_damage(attack_dmg)
 
 
-
-            # ON EN EST LA :
-
-
-  # d) On va coder une méthode 'attacks' qui permette de faire qu'un joueur attaque un autre. 
-  #Ainsi, si le joueur player1 attaque le joueur player2, on aurait juste à taper player1.attacks(player2):
-  def attacks(playerX)
-    puts "Et BIIIM! #{@name} attaque #{playerX.name} :"
-    attack_dmg = compute_damage
-    puts "l'attaque inflige #{attack_dmg} dégats !"
-  end # fin de la méthode 'attacks'
-
-  # on va créer une méthode à part s'appelant 'compute_damage' :
   def compute_damage
     return rand(1..6)
-  end
+  end # fin de la méthode 'compute_damage'
 
 
 
